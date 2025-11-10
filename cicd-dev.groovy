40bc0c7397cecd966d302e158f94d2018c9ab80b
sachint <32639496+sachintu47@users.noreply.github.com>
2025-11-10 18:08:08 +0530

    Fix GitHub repository URL in cicd.groovy
    Signed-off-by: sachint <32639496+sachintu47@users.noreply.github.com>

--- cicd-dev.groovy
+++ cicd-dev.groovy
@@ -2,11 +2,11 @@ node('linux') {
   stage ('Poll') {
     checkout([
       $class: 'GitSCM', branches: [[name: '*/main']], extensions: [],
-      userRemoteConfigs: [[url: 'https://github.com/zopencommunity/yaml-cppport.git']]])
+      userRemoteConfigs: [[url: 'https://github.com/zopencommunity/yamlcppport.git']]])
   }
   stage('Build') {
     build job: 'Port-Pipeline', parameters: [
-      string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/yaml-cppport.git'),
+      string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/yamlcppport.git'),
       string(name: 'PORT_DESCRIPTION', value: 'A C++ library for parsing and emitting YAML.'),
       string(name: 'BUILD_LINE', value: 'DEV')
     ]
